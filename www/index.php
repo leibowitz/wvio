6b933824fdd2772fe8d0526d8d21550e0f727497
Gianni Moschini <gianni.moschini@hailocab.com>
2013-11-02 15:45:56 +0000

    Add array of projects in config
    Set config for all projects in config file
    and then pick one

--- www/index.php
+++ www/index.php
@@ -2,25 +2,30 @@
 
 include __DIR__ . '/../vendor/autoload.php';
 
-use Github\Client;
 use Symfony\Component\Yaml\Yaml;
-
+use Guzzle\Http;
 
 // Get config from file
 $config_file = __DIR__ . '/../config/settings.yml';
 $config = Yaml::parse($config_file);
 
-// commit to look for
-$commit_version = '';
+$project = $config['projects'][ $config['project'] ];
+
+// find commit hash of deployed version
+$httpclient = new Http\Client($project['url']);
+$request = $httpclient->get();
+$resp = $request->send();
+$content = json_decode($resp->getBody(), true);
+$commit_version = $content[ $project['field'] ];
 
 // Authentification
-$client = new Client();
-$client->authenticate($config['token'], '', Client::AUTH_HTTP_TOKEN);
+$githubclient = new Github\Client();
+$githubclient->authenticate($config['token'], '', Github\Client::AUTH_HTTP_TOKEN);
 
 // start to query the github api about the commit
-echo sprintf("Looking for informations about commit %s\n", $commit_version);
+echo sprintf("Looking for informations about project %s commit %s\n", $project['name'], $commit_version);
 // get info about the commit
-$commit_details = $client->api('repo')->commits()->show($config['user'], $config['project'], $commit_version);
+$commit_details = $githubclient->api('repo')->commits()->show($config['user'], $project['name'], $commit_version);
 
 // display commit info
 echo sprintf("Author: %s\n", $commit_details['commit']['author']['name']);
@@ -29,7 +34,7 @@ echo sprintf("Message: %s\n", $commit_details['commit']['message']);
 
 // Get all branches
 echo "Retrieving branches information\n";
-$branches = $client->api('repo')->branches($config['user'], $config['project']);
+$branches = $githubclient->api('repo')->branches($config['user'], $project['name']);
 
 echo sprintf("Found %d branches\n", count($branches));
 
@@ -41,7 +46,7 @@ foreach($branches as $branch) {
     echo "Retrieving commits for branch: ".$branch['name']."\n";
 
     // Get latest 30 commits on this branch
-    $commits = $client->api('repo')->commits()->all($config['user'], $config['project'], array('sha' => $branch['commit']['sha']) );
+    $commits = $githubclient->api('repo')->commits()->all($config['user'], $project['name'], array('sha' => $branch['commit']['sha']) );
 
     echo sprintf("Found %d commits\n", count($commits));
 
