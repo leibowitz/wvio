0cbc5aa520c9701b90946bfc813f25729f37a33d
Gianni Moschini <gianni.moschini@hailocab.com>
2013-11-04 16:10:02 +0000

    Bring back logic to retrieve commit hash from api
    Read the response and extract commit hash from
    project and then use the github utils to get
    the branch for that commit

--- www/index.php
+++ www/index.php
@@ -5,6 +5,7 @@ include __DIR__ . '/../vendor/autoload.php';
 use Symfony\Component\Yaml\Yaml;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Guzzle\Http;
 use Leibowitz\Github\Utils\ProjectInfo;
 
 $environment = trim(file_get_contents( __DIR__ . '/../config/environment.txt' ));
@@ -18,6 +19,33 @@ $app = new Silex\Application();
 $app['debug'] = true;
 $app['info'] = new ProjectInfo($config);
 
+
+function getProjectStatusInfo($url)
+{
+    // find commit hash of deployed version
+    $httpclient = new Http\Client($url);
+
+    $request = $httpclient->get();
+    $resp = $request->send();
+
+    return json_decode($resp->getBody(), true);
+}
+
+function getProjectDetails($app, $project)
+{
+    $project_data = $app['info']->getProjectConfig($project);
+
+    $status = getProjectStatusInfo($project_data['url']);
+
+    $commit = $status[ $project_data['field'] ];
+
+    $details = $app['info']->getCommitDetails($commit, $project);
+
+    $details['branches'] = $app['info']->getBranchesForCommit($commit, $project);
+
+    return $details;
+}
+
 $app->match('{project}', function(Request $request) use ($app) {
     $resp = new Response();
 
@@ -27,7 +55,7 @@ $app->match('{project}', function(Request $request) use ($app) {
         $resp->setStatusCode(404);
         $payload = null;
     } else {
-        $payload = $app['info']->getProjectInfo($project);
+        $payload = getProjectDetails($app, $project);
     }
 
     $content = json_encode(
